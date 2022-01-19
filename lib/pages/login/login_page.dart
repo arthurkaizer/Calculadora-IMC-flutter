6529d4f23e70cd4f4d0f6eb59d3e8f4d85be6ac8
Arthur <arturkaizer@gmail.com>
2022-01-19 10:31:44 -0300

    style(login): implement screen

---
+++ lib/pages/login/login_page.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Center(
+        child: Column(
+          children: [
+            Padding(padding: EdgeInsets.only(top: 76)),
+            Image.asset(
+              'assets/images/logo_ioasys_1.png',
+              alignment: Alignment.topCenter,
+            ),
+            Padding(padding: EdgeInsets.only(bottom: 129)),
+            Container(
+                width: 300,
+                child: Text(
+                  "Seja bem vindo!",
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 31.0,
+                  ),
+                ),
+              ),
+              Container(
+                width: 300,
+                child: Text(
+                  "Calculadora IMC",
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24.0,
+                  ),
+                ),
+              ),
+            Padding(padding: EdgeInsets.only(bottom: 43)),
+             Container(
+                height: 50,
+                width: 300,
+                child: TextField(
+                  cursorColor: Colors.black,
+                  style: TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    contentPadding: EdgeInsets.fromLTRB(30, 20, 20, 20),
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(50)),
+                    hintStyle: TextStyle(
+                      color: Color(0xFF767676),
+                    ),
+                    hintText: "usuario",
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 50,
+                width: 300,
+                child: TextField(
+                  cursorColor: Colors.black,
+                  style: TextStyle(color: Colors.black),
+                  obscureText: true,
+                  obscuringCharacter: '*',
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    contentPadding: EdgeInsets.fromLTRB(30, 20, 20, 20),
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(50)),
+                    hintStyle: TextStyle(
+                      color: Color(0xFF767676),
+                    ),
+                    hintText: "senha",
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            Container(
+              width: 300,
+              height: 50,
+              child: ElevatedButton(
+                child: const Text('ENTRAR'),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/calculator');
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.black,
+                  textStyle: const TextStyle(
+                    fontSize: 16,
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
